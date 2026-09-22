650b3f4d367ea69429a0bdf19f6e9335242cdc33
Sathufit <sathush.nanayakkara04@gmail.com>
2026-09-22 12:28:37 +0530

    add qr library dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,4 +37,5 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
 }
