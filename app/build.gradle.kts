7f5746f85fa74c9da326d724b00b36c89695ff6f
Christine Lowe <shwethalowe@gmail.com>
2026-09-23 10:54:27 +0530

    show node details and slots on marker tap

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,5 +56,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
 }
